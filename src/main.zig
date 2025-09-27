2441ce05678549d565711e4f4df373d4d5949540
Jacob Sandlund <jacob@jacobsandlund.com>
2025-09-27 11:53:11 -0400

    remove unneded mod

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
-const copyv = @import("copyv");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try copyv.bufferedPrint();
 }
 
 test "simple test" {
