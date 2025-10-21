7388a10a395978963520ff07ce2b97701daf740e
Jacob Sandlund <jacob@jacobsandlund.com>
2025-10-21 08:45:33 -0400

    bunch of changes. invoking delta (temporarily)

---
+++ README.md
@@ -0,0 +1,11 @@
+# copyv
+
+Version control for copy paste!
+
+## Usage
+
+When copy pasting code from other repositories, add a comment above the code pointing back to the source and `copyv` can keep your copy pasted code up to date! (Well, that's the idea--right now it only shows a diff of what's been changed from when the code was copied.)
+
+```
+
+```
--- src/main.zig
+++ src/main.zig
@@ -2,35 +2,41 @@ const std = @import("std");
 
 const URL_TAG = "// copyv: ";
 
-fn doStuff(allocator: std.mem.Allocator, dir: std.fs.Dir) !void {
+fn recursivelyUpdate(arena: *std.heap.ArenaAllocator, dir: std.fs.Dir) !void {
     var it = dir.iterate();
-    var buff: [256]u8 = undefined;
     while (try it.next()) |entry| {
         if (entry.name.len > 1 and entry.name[0] == '.') continue;
 
         if (entry.kind == .directory) {
             var subdir = try dir.openDir(entry.name, .{ .iterate = true });
             defer subdir.close();
-            try doStuff(allocator, subdir);
+            try recursivelyUpdate(arena, subdir);
         } else if (entry.kind == .file) {
-            var file = try dir.openFile(entry.name, .{});
-            defer file.close();
-            var file_rdr = file.reader(&buff);
-            var rdr = &file_rdr.interface;
-            const bytes = try rdr.allocRemaining(allocator, .unlimited);
-            var lines = std.mem.splitScalar(u8, bytes, '\n');
-
-            while (lines.next()) |line| {
-                const trimmed = std.mem.trim(u8, line, " \t");
-                if (std.mem.startsWith(u8, trimmed, URL_TAG)) {
-                    try doMoreStuff(allocator, trimmed);
-                }
-            }
+            const allocator = arena.allocator();
+            try updateFile(allocator, dir, entry.name);
+            _ = arena.reset(.{ .retain_with_limit = 1024 * 1024 });
         }
     }
 }
 
-fn doMoreStuff(allocator: std.mem.Allocator, original_line: []const u8) !void {
+fn updateFile(allocator: std.mem.Allocator, dir: std.fs.Dir, name: []const u8) !void {
+    var file = try dir.openFile(name, .{});
+    defer file.close();
+    var buf: [4096]u8 = undefined;
+    var file_reader = file.reader(&buf);
+    var reader = &file_reader.interface;
+    const bytes = try reader.allocRemaining(allocator, .unlimited);
+    var lines = std.mem.splitScalar(u8, bytes, '\n');
+
+    while (lines.next()) |line| {
+        const trimmed = std.mem.trim(u8, line, " \t");
+        if (std.mem.startsWith(u8, trimmed, URL_TAG)) {
+            try updateChunk(allocator, trimmed);
+        }
+    }
+}
+
+fn updateChunk(allocator: std.mem.Allocator, original_line: []const u8) !void {
     var parts = std.mem.splitScalar(u8, original_line, ':');
     _ = parts.next(); // skip copyv
     const url_with_line_numbers = std.mem.trim(u8, parts.rest(), " \t");
@@ -52,8 +58,8 @@ fn doMoreStuff(allocator: std.mem.Allocator, original_line: []const u8) !void {
     const old_file_name = "tmp/old_file";
     const new_file_name = "tmp/new_file";
 
-    try fetchFile(allocator, old_url, old_file_name);
-    try fetchFile(allocator, new_url, new_file_name);
+    const old_file_bytes = try fetchFile(allocator, old_url, old_file_name);
+    const new_file_bytes = try fetchFile(allocator, new_url, new_file_name);
 
     var stderr = std.ArrayList(u8).empty;
     var stdout = std.ArrayList(u8).empty;
@@ -67,64 +73,138 @@ fn doMoreStuff(allocator: std.mem.Allocator, original_line: []const u8) !void {
     try child_proc.spawn();
     try child_proc.collectOutput(allocator, &stdout, &stderr, 1_000_000);
     const stdout_slice = try stdout.toOwnedSlice(allocator);
-    const exit_code = try child_proc.wait();
-    _ = exit_code;
+    _ = try child_proc.wait();
 
     var lines = std.mem.splitScalar(u8, line_numbers, '-');
-    const start_str = std.mem.trim(u8, lines.next().?, "L");
-    const end_str = std.mem.trim(u8, lines.next().?, "L");
-    const start = try std.fmt.parseInt(usize, start_str, 10);
-    const end = try std.fmt.parseInt(usize, end_str, 10);
+    const old_start_str = std.mem.trim(u8, lines.next().?, "L");
+    const old_end_str = std.mem.trim(u8, lines.next().?, "L");
+    const old_start = try std.fmt.parseInt(usize, old_start_str, 10);
+    const old_end = try std.fmt.parseInt(usize, old_end_str, 10);
 
     var diff_lines = std.mem.splitScalar(u8, stdout_slice, '\n');
-    var should_print = false;
+    var old_line: usize = 0;
+    var new_line: usize = 0;
+    var new_start: usize = 0;
+    var new_end: usize = 0;
+    var old_range: GitRange = .{ .start = 0, .len = 0 };
+    var new_range: GitRange = .{ .start = 0, .len = 0 };
+    for (0..4) |_| _ = diff_lines.next(); // Skip diff header
     while (diff_lines.next()) |line| {
         if (std.mem.startsWith(u8, line, "@@")) {
-            const trimmed = std.mem.trim(u8, line["@@".len..], " \t");
-            var header_parts = std.mem.splitScalar(u8, trimmed, ' ');
-            const old_range = header_parts.next().?;
-            var range_parts = std.mem.splitScalar(u8, old_range, ',');
-            const range_start_str = range_parts.next().?;
-            const diff_start = try std.fmt.parseInt(usize, range_start_str["-".len..], 10);
-            const range_len_str = range_parts.next().?;
-            const len = try std.fmt.parseInt(usize, range_len_str, 10);
-            const diff_end = diff_start + len;
+            std.debug.assert(old_line == old_range.start + old_range.len);
+            std.debug.assert(new_line == new_range.start + new_range.len);
+            var header_parts = std.mem.splitScalar(u8, line, ' ');
+            _ = header_parts.next().?; // skip @@
+            old_range = try parseRange(header_parts.next().?);
+            new_range = try parseRange(header_parts.next().?);
+            old_line = old_range.start;
+            new_line = new_range.start;
 
-            if ((diff_start <= start and end <= diff_end) or
-                (start <= diff_start and diff_end <= end) or
-                (diff_start < start and start < diff_end) or
-                (start < diff_start and diff_start < end))
-            {
-                should_print = true;
-            } else {
-                should_print = false;
+            if (new_start == 0 and old_line > old_start) {
+                const delta: usize = old_line - old_start;
+                new_start = new_line - delta;
+            } else if (new_end == 0 and old_line > old_end) {
+                const delta: usize = old_line - old_end;
+                new_end = new_line - delta;
             }
+        } else if (std.mem.startsWith(u8, line, "-")) {
+            old_line += 1;
+        } else if (std.mem.startsWith(u8, line, "+")) {
+            new_line += 1;
+        } else {
+            // Either it's a shared line or the last empty line of the diff
+            std.debug.assert(std.mem.startsWith(u8, line, " ") or diff_lines.peek() == null);
+            old_line += 1;
+            new_line += 1;
         }
 
-        if (should_print) {
-            std.debug.print("{s}\n", .{line});
+        if (old_line == old_start and new_start == 0) {
+            new_start = new_line;
+        }
+        if (old_line == old_end) {
+            new_end = new_line;
         }
     }
+
+    if (new_line == 0) {
+        // There was no diff
+        new_start = old_start;
+        new_end = old_end;
+    } else if (new_start == 0) {
+        // The only diffs were before this section
+        std.debug.assert(old_line < old_start);
+        const delta: isize = @intCast(new_line - old_line);
+        new_start = @intCast(@as(isize, @intCast(old_start)) + delta);
+        new_end = @intCast(@as(isize, @intCast(old_end)) + delta);
+    } else if (new_end == 0) {
+        // The only diffs were before the end of this section
+        std.debug.assert(old_line < old_end);
+        const delta: isize = @intCast(new_line - old_line);
+        new_end = @intCast(@as(isize, @intCast(old_end)) + delta);
+    }
+
+    try writeLines(old_file_bytes, old_file_name, old_start, old_end);
+    try writeLines(new_file_bytes, new_file_name, new_start, new_end);
+
+    child_proc = std.process.Child.init(
+        &[_][]const u8{ "delta", old_file_name, new_file_name },
+        allocator,
+    );
+    child_proc.stdout_behavior = .Inherit;
+    child_proc.stderr_behavior = .Inherit;
+    try child_proc.spawn();
+    _ = try child_proc.wait();
 }
 
-fn fetchFile(allocator: std.mem.Allocator, url: []const u8, path: []const u8) !void {
-    var file = try std.fs.cwd().createFile(path, .{});
-    defer file.close();
-    var buffer: [4096]u8 = undefined;
-    var file_writer = file.writer(&buffer);
-    const writer = &file_writer.interface;
+const GitRange = struct {
+    start: usize,
+    len: usize,
+};
 
+fn parseRange(range_str: []const u8) !GitRange {
+    var parts = std.mem.splitScalar(u8, range_str, ',');
+    const start_str = parts.next().?;
+    const start = try std.fmt.parseInt(usize, start_str[1..], 10);
+    const len_str = parts.next().?;
+    const len = try std.fmt.parseInt(usize, len_str, 10);
+    return .{ .start = start, .len = len };
+}
+
+fn fetchFile(allocator: std.mem.Allocator, url: []const u8, path: []const u8) ![]const u8 {
+    var aw = try std.Io.Writer.Allocating.initCapacity(allocator, 4096);
     var client = std.http.Client{ .allocator = allocator };
     _ = try client.fetch(.{
         .location = .{ .url = url },
-        .response_writer = writer,
+        .response_writer = &aw.writer,
     });
+
+    const bytes = try aw.toOwnedSlice();
+    try std.fs.cwd().writeFile(.{ .sub_path = path, .data = bytes });
+    return bytes;
+}
+
+fn writeLines(bytes: []const u8, path: []const u8, start_line: usize, end_line: usize) !void {
+    var start: usize = undefined;
+    var end: usize = undefined;
+    var lines = std.mem.splitScalar(u8, bytes, '\n');
+    var i: usize = 1;
+
+    while (lines.next()) |line| : (i += 1) {
+        if (i == start_line) {
+            start = line.ptr - bytes.ptr;
+        } else if (i == end_line) {
+            end = line.ptr - bytes.ptr + line.len;
+        }
+    }
+
+    try std.fs.cwd().writeFile(.{ .sub_path = path, .data = bytes[start..end] });
 }
 
 pub fn main() !void {
-    var debug_allocator = std.heap.DebugAllocator(.{}).init;
-    const allocator = debug_allocator.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
     var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
     defer dir.close();
-    try doStuff(allocator, dir);
+    try recursivelyUpdate(&arena, dir);
 }
