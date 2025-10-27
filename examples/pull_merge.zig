5665529203bc4dfee61ad476a8a993c30ddb0142
Jacob Sandlund <jacob@jacobsandlund.com>
2025-10-27 08:22:49 -0400

    fix pull_merge.zig accidental commit

--- examples/pull_merge.zig
+++ examples/pull_merge.zig
@@ -1,18 +1,15 @@
-// copyv: https://github.com/ghostty-org/ghostty/blob/3f75c66e8395d7389f05d360d89af567dcd22cba/src/benchmark/CodepointWidth.zig#L133-L163
+// copyv: https://github.com/ghostty-org/ghostty/blob/26e9b0a0f3b07149c7fd7474519eba6b21f8c5fd/src/benchmark/CodepointWidth.zig#L134-L165
 fn stepTable(ptr: *anyopaque) Benchmark.Error!void {
     const self: *CodepointWidth = @ptrCast(@alignCast(ptr));
 
     // I've added a comment here, this should stay.
     const f = self.data_f orelse return;
-    var read_buf: [4096]u8 = undefined;
-    var f_reader = f.reader(&read_buf);
-    var r = &f_reader.interface;
-
+    var r = std.io.bufferedReader(f.reader());
     var d: UTF8Decoder = .{};
-    var buf: [4096]u8 align(std.atomic.cache_line) = undefined;
+    var buf: [4096]u8 = undefined;
     while (true) {
-        const n = r.readSliceShort(&buf) catch {
-            log.warn("error reading data file err={?}", .{f_reader.err});
+        const n = r.read(&buf) catch |err| {
+            log.warn("error reading data file err={}", .{err});
             return error.BenchmarkFailed;
         };
         if (n == 0) break; // EOF reached
@@ -23,10 +20,14 @@ fn stepTable(ptr: *anyopaque) Benchmark.Error!void {
             if (cp_opt) |cp| {
                 // This is the same trick we do in terminal.zig so we
                 // keep it here.
-                std.mem.doNotOptimizeAway(if (cp <= 0xFF)
+                const width = if (cp <= 0xFF)
                     1
                 else
-                    table.get(@intCast(cp)).width);
+                    table.get(@intCast(cp)).width;
+
+                // Write the width to the buffer to avoid it being compiled
+                // away
+                buf[0] = @intCast(width);
             }
         }
     }
